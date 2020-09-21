b4616587b358d2fcfb0ffe4fb78879578e5ad901
linux_china <libing.chen@gmail.com>
2020-09-21 00:27:47 -0700

    add test mod

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -2,10 +2,24 @@ use neon::prelude::*;
 
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     let name = cx.argument::<JsString>(0)?.value();
-    let result = cx.string(format!("hello {}!", name));
+    let result = cx.string(hello_impl(&name));
     Ok(result)
+}
+
+fn hello_impl(name: &str) -> String {
+    format!("hello {}!", name)
 }
 
 register_module!(mut cx, {
     cx.export_function("hello", hello)
 });
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hello() {
+        println!("{}", hello_impl("Jackie"))
+    }
+}
